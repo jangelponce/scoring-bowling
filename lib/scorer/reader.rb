6ed8fece66f88998d9b1a602bba7a16c0946644b
Angel Ponce <j.angelponce.esc@gmail.com>
2020-12-06 18:21:34 -0700

    Adding class Reader that inherits from CSV for convenience

---
+++ lib/scorer/reader.rb
@@ -0,0 +1,23 @@
+class Reader < CSV
+  class << self
+    def open file, **kwargs
+      options = { 
+        col_sep: "\t" 
+      }.update(kwargs)
+      
+      super file, options
+    end
+
+    def read file
+      open(file).to_h
+    end
+  end
+  
+  def to_h 
+    keys = [:name, :pins]
+
+    map do |a| 
+      Hash[ keys.zip(a) ]
+    end
+  end
+end
