50586849e2c6e3deda9f9c9550b5c997d39fd3fd
Angel Ponce <j.angelponce.esc@gmail.com>
2020-12-06 18:28:52 -0700

    Maybe is better aproach getting all classes inside a module

--- lib/scorer.rb
+++ lib/scorer.rb
@@ -1,6 +1,12 @@
 require "csv"
 require_relative "scorer/reader.rb"
 
-class Scorer
-  
+module Scorer
+  class Base
+    attr_accessor :scores
+
+    def initialize scores = []
+      @scores = scores
+    end
+  end
 end
--- lib/scorer/reader.rb
+++ lib/scorer/reader.rb
@@ -1,23 +1,25 @@
-class Reader < CSV
-  class << self
-    def open file, **kwargs
-      options = { 
-        col_sep: "\t" 
-      }.update(kwargs)
+module Scorer
+  class Reader < CSV
+    class << self
+      def open file, **kwargs
+        options = { 
+          col_sep: "\t" 
+        }.update(kwargs)
+        
+        super file, options
+      end
       
-      super file, options
+      def read file
+        open(file).to_h
+      end
     end
-
-    def read file
-      open(file).to_h
-    end
-  end
-  
-  def to_h 
-    keys = [:name, :pins]
-
-    map do |a| 
-      Hash[ keys.zip(a) ]
+    
+    def to_h 
+      keys = [:name, :pins]
+      
+      map do |a| 
+        Hash[ keys.zip(a) ]
+      end
     end
   end
 end
